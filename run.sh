96312a09e9ce2e794a4d8eae34f404eca0fa9f6d
Haim Grosman <korovkin@gmail.com>
2021-07-09 13:10:01 -0700

    cleanup

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-nvidia-smi -pm 1
-nvidia-smi -i 0 -pl 230
-while [ yes ] ; do ./ethminer/ethminer -U -P stratum://0x86e52935489C9BB39dEfCEAd34F191A279417eee@us2.ethermine.org:4444 -v 8 ; sleep 4; date; done
+# sudo nvidia-smi -pm 1 && sudo nvidia-smi -i 0 -pl 210
+cd ~/v/ethminer/
+while [ yes ] ; do ./_build/ethminer/ethminer -U -P stratum://0x86e52935489C9BB39dEfCEAd34F191A279417eee@us2.ethermine.org:4444 -v 8 ; sleep 4; date; done
